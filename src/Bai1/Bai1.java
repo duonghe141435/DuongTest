4d27037dfdbbf26195f4036973446a2863552bc1
duonghe141435 <87297848+duonghe141435@users.noreply.github.com>
2022-02-23 10:43:29 +0700

    Update Bai1.java

--- src/Bai1/Bai1.java
+++ src/Bai1/Bai1.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Bai1;
+package bai1;
 
 import Validate.Validate;
 import java.util.Scanner;
